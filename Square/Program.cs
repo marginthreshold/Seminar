a57ce4fae17a4757b630997595c23d1fe9d92102
Lavanov <alavanov@rencap.com>
2022-09-15 20:32:47 +0300

    Find Square

---
+++ Square/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2=int.Parse(Console.ReadLine()!);
+int square2=Convert.ToInt32(Math.Pow(number2,2));
+int square = number1 * number1;
+Console.WriteLine("Квадрат числа равен " + square);
+Console.WriteLine($"Квадрат числа {number2} равен {square2}");
