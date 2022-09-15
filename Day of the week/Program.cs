a1a86219564dec004bffe97e0c2cefcf3de03631
Lavanov <alavanov@rencap.com>
2022-09-15 21:21:05 +0300

    Day of the week

---
+++ Day of the week/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+WriteLine("Введите день недели от 1 до 7:");
+int DayOfWeek = Convert.ToInt32(ReadLine());
+string Day ="Неправильно ввели день недели";
+
+if(DayOfWeek==1) Day= "Понедельник";
+if(DayOfWeek==2) Day= "Вторник";
+if(DayOfWeek==3) Day= "Среда";
+if(DayOfWeek==4) Day= "Четверг";
+if(DayOfWeek==5) Day= "Пятница";
+if(DayOfWeek==6) Day= "суббота";
+if(DayOfWeek==7) Day= "Воскресенье";
+
+
+WriteLine(Day);
