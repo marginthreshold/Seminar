6f106a2668f85fc217184f580e0c05d08daea152
Lavanov <alavanov@rencap.com>
2022-09-15 20:57:00 +0300

    Check Square

---
+++ Check square/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+
+Clear();
+WriteLine("Введите число");
+int number1 = Convert.ToInt32(ReadLine());
+WriteLine("Введите квадрат числа");
+int number2=int.Parse(ReadLine()!);
+
+int square2=Convert.ToInt32(Math.Pow(number1,2));
+//int square = number1 * number1;
+//WriteLine("Квадрат числа равен " + square);
+if (number2==square2)
+{
+ WriteLine("да");   
+}
+else
+{
+ WriteLine("нет");   
+}
+
+
+
