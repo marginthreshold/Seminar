ad841c194c35949e0cd2f4bd34b7a11c385cbdf8
Lavanov <alavanov@rencap.com>
2022-09-19 20:16:16 +0300

    Random 10-99

---
+++ Random (1-99)/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+using static System.Console;
+Clear();
+
+
+int number=new Random().Next(10,100);
+WriteLine(number);
+
+int x= number%10;
+int y=(number-x)/10;
+
+int max=Math.Max(x,y);
+WriteLine(max);
