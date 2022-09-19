dc82fc3054542aa2fc6e2cc400701da63fdce7f1
Lavanov <alavanov@rencap.com>
2022-09-19 21:44:44 +0300

    Revert Triple

---
+++ Revert Triple/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Clear();
+
+int number=new Random().Next(100,1000);
+WriteLine(number);
+int x= number/100;
+int y=(number - x*100)/10;
+int z=number%10;
+
+if (z==0) WriteLine("0"y*10+x);
+else
+{
+WriteLine(z*100+y*10+x);
+}
