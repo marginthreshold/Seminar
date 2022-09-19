ed7ef8e0b7c02d93d2433c64a696e0a9d008582e
Lavanov <alavanov@rencap.com>
2022-09-19 21:29:58 +0300

    Two numbers Square

---
+++ Two numbers Sqare or not/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+using static System.Console;
+Clear();
+
+int x = int.Parse(ReadLine()!);
+int y = int.Parse(ReadLine()!);
+
+if (x<y)
+{
+    int temp =x;
+    x=y;
+    y=temp;
+}
+
+if(x==y*y) 
+{
+    WriteLine($"{x} является квадратом {y}");
+}
+else
+{
+ WriteLine($"{x} не является квадрато{y}");   
+}
