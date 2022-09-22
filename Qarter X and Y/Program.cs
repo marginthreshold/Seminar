059d839a134b3180ca74ef92db01244fb5a350fe
Lavanov <alavanov@rencap.com>
2022-09-22 20:31:06 +0300

    Qarter X and Y

---
+++ Qarter X and Y/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите X ");
+int X=int.Parse(ReadLine()!);
+
+WriteLine("Введите Y ");
+int Y=int.Parse(ReadLine()!);
+
+if (X==0 || Y==0)
+{
+WriteLine("Не должен быть равен нулю");
+return;
+}
+
+
+if(X>0 && Y>0)
+{
+    WriteLine("Первая четверть");
+}
+if(X<0 && Y>0)
+{
+    WriteLine("Вторая четврть");
+}
+if(X<0 && Y<0)
+{
+    WriteLine("Третья четверть");
+}
+if(X>0 && Y<0)
+{
+    WriteLine("Четвертая четверть");
+}
